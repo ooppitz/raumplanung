9bccd1c344fc0ea346069eec8ee45af6da2710d6
oppitz_oliver <oppitz_oliver@192.168.56.1>
2021-07-22 12:56:27 +0200

    Fixed module-info.java

--- raumplanung/src/main/java/module-info.java
+++ raumplanung/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module diegfi.raumplanung {
     requires javafx.controls;
     requires javafx.fxml;
-	requires org.junit.jupiter.api;
 
     opens diegfi.raumplanung;
     exports diegfi.raumplanung;
